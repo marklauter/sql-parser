b9c6ef736bc94812a384bdb84f83f64e2e38126d
Mark Lauter <mark@marklauter.com>
2024-06-16 21:23:49 -0400

    better tests

--- Squeal.Tests/SelectStatementParserTests.cs
+++ Squeal.Tests/SelectStatementParserTests.cs
@@ -5,12 +5,21 @@ namespace Squeal.Tests;
 
 public sealed class SelectStatementParserTests
 {
-    [Fact]
-    public void ParseSelectStatement()
+    [Theory]
+    [InlineData("SELECT name FROM apples", 1)]
+    [InlineData("SELECT name, id FROM apples", 2)]
+    public void ParseSelectStatement(string sql, int expectedColumnCount)
     {
-        var sql = "SELECT name FROM apples";
         var tokens = Sql.Tokenizer.Tokenize(sql);
         var result = Sql.SelectStatement.TryParse(tokens);
+        var statement = result.Value;
         Assert.True(result.HasValue, result.ToString());
+        Assert.Equal("apples", statement.TableName.Name);
+        Assert.Equal(expectedColumnCount, statement.Columns.Length);
+        Assert.Equal("name", statement.Columns[0].Name);
+        if(expectedColumnCount == 2)
+        {
+            Assert.Equal("id", statement.Columns[1].Name);
+        }
     }
 }
